c02cd57fad6d1b0e87668c9ed10061527219ea94
kazusa_pg <kazusaprog@gmail.com>
2020-04-18 22:46:22 +0900

    First Commit

---
+++ ITP1_1_C/ITP1_1_C/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ITP1_1_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ab = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+            var a = ab[0];
+            var b = ab[1];
+            Console.Write(a * b);
+            Console.Write(" ");
+            Console.WriteLine(a * 2 + b * 2);
+        }
+    }
+}
